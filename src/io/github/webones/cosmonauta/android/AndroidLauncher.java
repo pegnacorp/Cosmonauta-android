db8afeb7cc2a87df9896abe213d792df99782387
Arandi Lopez <arandilopez.93@gmail.com>
2014-12-08 16:00:47 -0600

    se pone en vertical la pantalla

--- src/io/github/webones/cosmonauta/android/AndroidLauncher.java
+++ src/io/github/webones/cosmonauta/android/AndroidLauncher.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
 import io.github.webones.cosmonauta.CosmonautaGame;
+import io.github.webones.cosmonauta.views.GameScreen;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new CosmonautaGame(), config);
+//		((Object) config).useAccelerometer(true);
+		initialize(new GameScreen(), config);
 	}
 }
