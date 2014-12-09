317ac59f659c0ac1ee3704879026be2d8302a8e9
Arandi Lopez <arandilopez.93@gmail.com>
2014-12-08 19:40:48 -0600

    lol p

--- src/io/github/webones/cosmonauta/android/AndroidLauncher.java
+++ src/io/github/webones/cosmonauta/android/AndroidLauncher.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
+import io.github.webones.cosmonauta.Cosmonauta;
 import io.github.webones.cosmonauta.CosmonautaGame;
 import io.github.webones.cosmonauta.views.GameScreen;
+import io.github.webones.cosmonauta.views.IntroScreen;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
@@ -14,6 +16,8 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 //		((Object) config).useAccelerometer(true);
-		initialize(new GameScreen(), config);
+//		initialize(new GameScreen(), config);
+		initialize(new Cosmonauta(), config);
+//		initialize(new IntroScreen(), config);
 	}
 }
